56a7ff21b6a56ea9c54b4620e8e79791469e036e
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-08 00:04:55 +0200

    add pulsating animation to circle

--- Animated Progress Bar/ViewController.swift
+++ Animated Progress Bar/ViewController.swift
@@ -12,36 +12,64 @@ import UIKit
 class ViewController: UIViewController, URLSessionDownloadDelegate {
     let shapeLayer = CAShapeLayer()
     
+    var pulsatingLayer: CAShapeLayer!
+    
     let percentageLabel: UILabel = {
         let label = UILabel()
         label.text = "Start"
         label.textAlignment = .center
         label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.textColor = .white
         return label
     }()
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
+    private func setupNotificationObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+    }
+    
+    @objc private func handleEnterForeground(){
+        animatePulsatingLayer()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(percentageLabel)
-        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        percentageLabel.center = view.center
+        setupNotificationObservers()
+        
+        
+        view.backgroundColor = UIColor.backgroundColor
         // drawing a circle
         let trackLayer = CAShapeLayer()
         let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        trackLayer.path = circularPath.cgPath
+        //create pulsating layer
+        pulsatingLayer = CAShapeLayer()
+        pulsatingLayer.path = circularPath.cgPath
+        pulsatingLayer.strokeColor = UIColor.clear.cgColor
+        pulsatingLayer.lineWidth = 10
+        pulsatingLayer.fillColor = UIColor.pulsatingFillColor.cgColor
+        pulsatingLayer.lineCap = CAShapeLayerLineCap.round
+        pulsatingLayer.position = view.center
+        view.layer.addSublayer(pulsatingLayer)
         
-        trackLayer.strokeColor = UIColor.lightGray.cgColor
-        trackLayer.lineWidth = 10
-        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = UIColor.trackStrokeColor.cgColor
+        trackLayer.lineWidth = 20
+        trackLayer.fillColor = UIColor.backgroundColor.cgColor
         trackLayer.lineCap = CAShapeLayerLineCap.round
         trackLayer.position = view.center
         view.layer.addSublayer(trackLayer)
         
+       
+        
+        animatePulsatingLayer()
+        
         // create track layer
         shapeLayer.path = circularPath.cgPath
-        
-        shapeLayer.strokeColor = UIColor.red.cgColor
-        shapeLayer.lineWidth = 10
+        shapeLayer.strokeColor = UIColor.outlineStrokeColor.cgColor
+        shapeLayer.lineWidth = 20
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineCap = CAShapeLayerLineCap.round
         shapeLayer.position = view.center
@@ -53,6 +81,20 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         view.layer.addSublayer(shapeLayer)
         
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
+        
+        view.addSubview(percentageLabel)
+        percentageLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        percentageLabel.center = view.center
+    }
+    private func animatePulsatingLayer(){
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.3
+        animation.duration = 0.8
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses = true
+        animation.repeatCount = Float.infinity
+        pulsatingLayer.add(animation, forKey: "pulsing")
     }
     
     let urlString = "https://firebasestorage.googleapis.com/v0/b/firestorechat-e64ac.appspot.com/o/intermediate_training_rec.mp4?alt=media&token=e20261d0-7219-49d2-b32d-367e1606500c"
