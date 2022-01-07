86daa034bae387b44ffd6b8adb95fec70868fb12
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-07 20:12:27 +0200

    add animation stroke

--- Animated Progress Bar/ViewController.swift
+++ Animated Progress Bar/ViewController.swift
@@ -7,18 +7,35 @@
 
 import UIKit
 
+let shapeLayer = CAShapeLayer()
+
 class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // drawing a circle
-        let shapeLayer = CAShapeLayer()
+        
         let center = view.center
         let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         shapeLayer.path = circularPath.cgPath
+        
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 10
+        
+        shapeLayer.strokeEnd = 0
         view.layer.addSublayer(shapeLayer)
+        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
-    
-    
+    @objc private func handleTap(){
+        print ("Walahala!!")
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        basicAnimation.toValue = 1
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+    }
 }
+
 
