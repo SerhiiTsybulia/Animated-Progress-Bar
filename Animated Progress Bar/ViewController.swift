bb413f777589e5294a28f4eabc93327058035594
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-07 19:39:24 +0200

    Initial Commit

---
+++ Animated Progress Bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animated Progress Bar
+//
+//  Created by Сергей on 07.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
