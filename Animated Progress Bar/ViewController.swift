9a75fde51bfd76fc9cc471eac15db946f9a716af
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-07 19:54:44 +0200

    drawing a circle

--- Animated Progress Bar/ViewController.swift
+++ Animated Progress Bar/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // drawing a circle
+        let shapeLayer = CAShapeLayer()
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        view.layer.addSublayer(shapeLayer)
     }
-
-
+    
+    
 }
 
