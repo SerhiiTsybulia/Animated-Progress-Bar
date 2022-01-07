7ad34829c39c2d75ae31d5af9c724906b01f2a2e
SergeiTsybulya <sergtsybulya@gmail.com>
2022-01-07 20:28:39 +0200

    create track layer

--- Animated Progress Bar/ViewController.swift
+++ Animated Progress Bar/ViewController.swift
@@ -16,25 +16,46 @@ class ViewController: UIViewController {
         // drawing a circle
         
         let center = view.center
-        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        // create track layer
+        let trackLayer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+        
+        trackLayer.strokeColor = UIColor.lightGray.cgColor
+        trackLayer.lineWidth = 10
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineCap = CAShapeLayerLineCap.round
+        view.layer.addSublayer(trackLayer)
+        
+        
         shapeLayer.path = circularPath.cgPath
         
         shapeLayer.strokeColor = UIColor.red.cgColor
         shapeLayer.lineWidth = 10
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
+        
         
         shapeLayer.strokeEnd = 0
+        
         view.layer.addSublayer(shapeLayer)
         
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     @objc private func handleTap(){
         print ("Walahala!!")
-        
+        // add animation
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         
         basicAnimation.toValue = 1
         
-        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+        basicAnimation.duration = 2
+        
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "Basic")
     }
 }
 
